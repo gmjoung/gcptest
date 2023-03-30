2ae81e52d3d2045efa59df4d6e794468fb2bcd4a
gmjoung <129419855+gmjoung@users.noreply.github.com>
2023-03-30 21:36:10 +0100

    Creation of BQ dataset, table, some data into project-test1

---
+++ main.tf
@@ -0,0 +1,52 @@
+# Connecting to Project
+provider "google" {
+  project = "project-test1"
+  region  = "europe-west1"
+}
+
+# Creating BQ Dataset
+resource "google_bigquery_dataset" "my_dataset" {
+  dataset_id                  = "dataset1"
+  friendly_name               = "Gumin Dataset"
+  description                 = "test dataset to see if it can be integrated into Vertex AI"
+  location                    = "EU"
+  default_table_expiration_ms = "3600000" #100 hours expiration time
+}
+
+# Setting up table in BQ dataset
+resource "google_bigquery_table" "my_table" {
+  dataset_id = google_bigquery_dataset.my_dataset.dataset_id
+  table_id   = "table1"
+
+  schema {
+    fields {
+      name = "name"
+      type = "STRING"
+    }
+    fields {
+      name = "age"
+      type = "INTEGER"
+    }
+  }
+}
+
+# Adding basic data to created table in BQ dataset
+resource "google_bigquery_table_data_insert_all" "my_table_data" {
+  dataset_id = google_bigquery_dataset.my_dataset.dataset_id
+  table_id   = google_bigquery_table.my_table.table_id
+
+  rows = [
+    {
+      "json" = {
+        "name" = "Gumin",
+        "age"  = 27
+      }
+    },
+    {
+      "json" = {
+        "name" = "Arpan",
+        "age"  = 26
+      }
+    }
+  ]
+}
