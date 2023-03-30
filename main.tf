adc89759fdaff7de704aa163e9f11f3366cf2ecf
gmjoung <129419855+gmjoung@users.noreply.github.com>
2023-03-30 22:36:33 +0100

    removing commented code

--- main.tf
+++ main.tf
@@ -36,16 +36,3 @@ resource "google_bigquery_table" "my_table" {
 EOF
 
 }
-
-# Adding basic data to created table in BQ dataset !removed for now
-#resource "google_bigquery_table_data" "my_table_data" {
-#   dataset_id = google_bigquery_dataset.my_dataset.dataset_id
-#   table_id   = google_bigquery_table.my_table.table_id
-
-#   source_format = "NEWLINE_DELIMITED_JSON"
-
-#   json = <<JSON
-#   { "name": "Gumin", "age": 27 }
-#   { "name": "Arpan2", "age": 26 }
-#   JSON
-# }
